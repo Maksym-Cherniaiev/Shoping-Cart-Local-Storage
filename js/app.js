27445a32b1904ac16f9f12f0e1ad731b23ab8111
Maksym <likeran2@gmail.com>
2019-07-16 19:12:49 +0300

    added script and class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 const listOfCourses = document.getElementById("courses-list");
+listOfCourses.addEventListener("click", addCourse);
+
+class CourseHandler {
+  constructor(e) {
+    this.event = e;
+    this.handleCourse();
+  }
+
+  handleCourse() {
+    if (this.event.target.classList.contains("add-to-cart")) {
+      console.log(this.event.target);
+    }
+  }
+}
+
+function addCourse(e) {
+  const newCourse = new CourseHandler(e);
+}
